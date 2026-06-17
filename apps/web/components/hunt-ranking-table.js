076670231c62e4868abffbf8dc6a6a07b442d63c
umeda <umedademu@gmail.com>
2026-06-17 23:54:42 +0900

    Aパーク春日北斗転生共通1位を期待値へ反映

--- apps/web/components/hunt-ranking-table.js
+++ apps/web/components/hunt-ranking-table.js
@@ -481,6 +481,7 @@ const A_PARK_KASUGA_COMMON_HUNT_SCORE_TOP_EXPECTATION_MACHINE_NAMES = new Set([
   "ミスタージャグラー",
   "ネオアイムジャグラーEX",
   "マイジャグラーV",
+  "スマスロ北斗の拳 転生の章",
 ]);
 
 function shouldUseCommonHuntScoreMachineTopExpectation(storeId, storeName, machineName) {
